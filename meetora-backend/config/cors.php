bcec9d9e2ec7e1a3cd7bd0d8528395facf4a9be2
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-09-12 20:56:34 +0100

    configered frontend url

--- meetora-backend/config/cors.php
+++ meetora-backend/config/cors.php
@@ -1,5 +1,37 @@
 <?php
 
+// return [
+
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => [
+//         env('FRONTEND_URL', 'http://localhost:5173'),
+//         // 'https://meetora-frontend.onrender.com',
+//     ],
+//     // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     // 'allowed_origins' => [
+//     //     'https://meetoraa.tech',
+//     //     'https://www.meetoraa.tech',
+//     // ],
+
+//     // 'supports_credentials' => true,
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+
+// ];
+
+
 return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
@@ -7,17 +39,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        // 'https://meetora-frontend.onrender.com',
+        env('FRONTEND_URL', 'https://meetora-frontend.onrender.com'),
     ],
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    // 'allowed_origins' => [
-    //     'https://meetoraa.tech',
-    //     'https://www.meetoraa.tech',
-    // ],
-
-    // 'supports_credentials' => true,
 
     'allowed_origins_patterns' => [],
 
