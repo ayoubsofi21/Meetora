aeb8da034f4683b4af785a7743c5eadc5b8c926d
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-09-14 16:43:27 +0100

    allow host Render host in vite config

--- meetora-backend/vite.config.js
+++ meetora-backend/vite.config.js
@@ -1,3 +1,27 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+//     // server: {
+//     //     watch: {
+//     //         ignored: ['**/storage/framework/views/**'],
+//     //     },
+//     // },
+//     server: {
+//         host: '0.0.0.0',
+//         allowedHosts: [
+//         'meetora-frontend.onrender.com',
+//         ],
+//   },
+// });
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,20 +29,17 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
-    // server: {
-    //     watch: {
-    //         ignored: ['**/storage/framework/views/**'],
-    //     },
-    // },
+
     server: {
         host: '0.0.0.0',
-        allowedHosts: [
-        'meetora-frontend.onrender.com',
-        ],
-  },
+        allowedHosts: true,
+    },
 });
