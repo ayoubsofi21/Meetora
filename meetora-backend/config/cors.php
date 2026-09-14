07e56593382afbb60363453e40e36ddcb5f5c81a
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-09-14 22:08:17 +0100

    config cors

--- meetora-backend/config/cors.php
+++ meetora-backend/config/cors.php
@@ -32,14 +32,40 @@
 // ];
 
 
+// return [
+
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => [
+//         env('VITE_API_BASE_URL', 'https://meetora-frontend.onrender.com'),
+//     ],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+//     'APP_ENV'=>'production',
+//     'APP_DEBUG'=>false,
+//     'APP_URL'=>'https://meetora-nu9q.onrender.com',
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+
+// ];
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'https://meetora-frontend.onrender.com'),
+        env('FRONTEND_URL', 'http://localhost:5173'),
     ],
 
     'allowed_origins_patterns' => [],
